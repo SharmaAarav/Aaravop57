abc5a39794a1cca8b67bb23adce882c61496146f
SharmaAarav <98636041+SharmaAarav@users.noreply.github.com>
2022-03-26 12:40:20 +0530

    commit - AaravSharma commit

---
+++ app/src/main/java/edu/aaravsharma/androidworkshop2022/AaravSharma.java
@@ -0,0 +1,14 @@
+package edu.aaravsharma.androidworkshop2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AaravSharma extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aarav_sharma2);
+    }
+}
